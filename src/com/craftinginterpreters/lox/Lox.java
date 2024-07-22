6645d51e25a650d0d7a1a239f1e0bbddfaa5434d
Ravi_Almeida <ravivendramini@gmail.com>
2024-07-22 18:42:46 -0300

    feat: implement interpreter

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -10,7 +10,10 @@ import java.util.List;
 
 public class Lox {
     
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
+    
     
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -28,6 +31,7 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
         
         if (hadError) System.exit(65);
+        if (hadRuntimeError) System.exit(70);
     }
     
     private static void runPrompt() throws IOException {
@@ -51,11 +55,7 @@ public class Lox {
         
         if (hadError) return;
         
-        System.out.println(new AstPrinter().print(expression));
-//        
-//        for (Token token : tokens){
-//            System.out.println(token);
-//        }
+        interpreter.interpret(expression);
     }
     
     static void error (int line, String message){
@@ -74,5 +74,10 @@ public class Lox {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
     }   
+    
+    static void runtimeError(RuntimeError error){
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
+    }
 }
 
