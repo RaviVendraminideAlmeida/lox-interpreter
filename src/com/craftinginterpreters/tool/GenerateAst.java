ba71c4f39bb7ccab50e45fce66ec1018f10cbade
Ravi_Almeida <ravivendramini@gmail.com>
2024-07-23 15:37:41 -0300

    lol: trynna implement variable statements

--- src/com/craftinginterpreters/tool/GenerateAst.java
+++ src/com/craftinginterpreters/tool/GenerateAst.java
@@ -8,23 +8,28 @@ import java.util.List;
 public class GenerateAst {
 
     public static void main(String[] args) throws IOException {
+        
+        /*
         if (args.length != 1) {
             System.err.println("Usage: generate_ast <output directory>");
             System.exit(64);
         }
+        */
 
-        String outputDir = args[0];
+        String outputDir = "C:\\Users\\infoSiS-Prog1\\Documents\\NetBeansProjects\\LoxInterpreter\\src\\com\\craftinginterpreters\\lox\\"; // args[0];
 
         defineAst(outputDir, "Expr", Arrays.asList(
                 "Binary   : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
                 "Literal  : Object value",
-                "Unary    : Token operator, Expr right"
+                "Unary    : Token operator, Expr right",
+                "Variable : Token name"
         ));
 
         defineAst(outputDir, "Stmt", Arrays.asList(
                 "Expression : Expr expression",
-                "Print      : Expr expression"
+                "Print      : Expr expression",
+                "Var        : Token name, Expr initializer"
         ));
         
         
